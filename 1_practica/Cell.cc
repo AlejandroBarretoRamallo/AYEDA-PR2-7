df4d6633dbf63ec8895b6d9e174bc851c1298502
alu0101552774 <alu0101552774@ull.edu.es>
2024-02-16 12:33:32 +0100

    a

---
+++ 1_practica/Cell.cc
@@ -0,0 +1,115 @@
+#include "Cell.h"
+
+Cell::Cell(const Position& position, const  State& state) {
+  state_ = state;
+  position_ = position;
+  State next_state(0);
+  next_state_ = next_state;
+}
+
+Cell::Cell() {
+  position_ = Position();
+  state_ = State();
+  next_state_ = State(0);
+}
+
+State Cell::getState() const {
+  return state_;
+}
+
+void Cell::updateState() {               
+  state_ = next_state_;
+}
+
+std::ostream& operator<<(std::ostream& out, const Cell& cell) {
+  if(cell.getState().getstate()) {
+    std::cout << "X";
+  }
+  else {
+    std::cout << " ";
+  }
+  return out;
+}
+
+int Cell::NextState(Lattice& reticulo) const{
+  int vecindad = 0;
+  if (IsBorder(reticulo) && reticulo.getBorderType() == "periodica") {     
+   if (position_.getPosition() == 0) {
+      int numero = position_.getPosition();
+      Position posicion(reticulo.getSize() - 1);           
+      vecindad += reticulo.getCell(posicion).getState().getstate() * 100;
+      vecindad += state_.getstate() * 10;
+      numero = position_.getPosition();
+      Position posicion2(numero + 1);           
+      vecindad += reticulo.getCell(posicion2).getState().getstate();
+    }
+    else {
+      int numero = position_.getPosition();
+      Position posicion(0);           
+      vecindad += reticulo.getCell(posicion).getState().getstate();
+      vecindad += state_.getstate() * 10;
+      numero = position_.getPosition() - 1;
+      Position posicion2(numero);           
+      vecindad += reticulo.getCell(posicion2).getState().getstate() * 100;
+    }
+  }
+  else {   
+    Position position(position_.getPosition() -1);
+    int numero = reticulo.getCell(position).getState().getstate();
+    vecindad += numero * 100;  // obtenemos el estado de la celula anterior
+    numero = state_.getstate();
+    vecindad += numero * 10;      // obtenemos el estado de la celula actual
+    numero = position_.getPosition();
+    Position posicion(numero + 1);           
+    vecindad += reticulo.getCell(posicion).getState().getstate();
+  }
+  switch(vecindad) {    // calculamos el estado dependiendo de la vecindad
+    case 0:
+      return 0;
+      break;
+    case 1:
+      return 1;
+      break;
+    case 10:
+      return 1;
+      break;
+    case 11:
+      return 1;
+      break;
+    case 100:
+      return 0;
+      break;
+    case 101:
+      return 1;
+      break;
+    case 110:
+      return 1;
+      break;
+    case 111:
+      return 0; 
+      break;
+    default:
+      return 0;
+  }
+}
+
+bool Cell::IsBorder(Lattice& reticulo) const {
+  if (position_.getPosition() == 0 || position_.getPosition() == reticulo.getSize() - 1) {
+    return true;
+  }
+  return false;
+}
+
+std::ostream& operator<<(std::ostream& out, Cell& celula) {
+  if (celula.getState().getstate() == 1) {
+    std::cout << "X";
+  }
+  else {
+    std::cout << " ";
+  }
+  return out;
+}
+
+void Cell::setNextState(const int& state) {
+  next_state_ = State(state);
+}
---
+++ 1_practica/Lattice.cc
@@ -0,0 +1,97 @@
+#include "Lattice.h"
+
+Lattice::Lattice() {
+  celulas_ = new Cell[1];
+  size_ = 1;
+  Position posicion;
+  State state;
+  celulas_[0] = Cell(posicion, state);
+  std::string borderType_ = "caliente";
+  n_generation_ = 1;
+}
+
+
+Lattice::Lattice(const std::string& configuracion, const std::string& frontera) {
+  n_generation_ = 1;
+  const int tamaño_reticulo = configuracion.size() + 2;
+  if (frontera != "periodica") {
+    size_ = tamaño_reticulo;
+    celulas_ = new Cell[tamaño_reticulo];
+    Position position(0), position2(tamaño_reticulo - 1);
+    State state;
+    if (frontera == "caliente") {  
+      state.setState(1);
+    }
+    else {
+      state.setState(0);
+    }
+    celulas_[0] = Cell(position, state); 
+    celulas_[tamaño_reticulo - 1] = Cell(position2, state);
+    for (int i = 0; i < configuracion.size(); ++i) {
+      Position position(i + 1);
+      int estado = configuracion[i] - '0';
+      State state(estado);
+      celulas_[i + 1] = Cell(position, state);
+    }
+  }
+  else {
+    size_ = configuracion.size();
+    celulas_ = new Cell[size_];
+    for (int i = 0; i < size_; ++i) {
+      Position position(i);
+      int estado = configuracion[i] - '0';
+      State state(estado);
+      celulas_[i] = Cell(position, state);
+    }
+  }
+  borderType_ = frontera;
+}
+
+const Cell& Lattice::getCell(const Position& position) const {
+  return celulas_[position.getPosition()];
+}
+
+Cell* Lattice::getCelulas() {
+  return celulas_;
+}
+
+int Lattice::getSize() const {
+  return size_;
+}
+
+std::ostream& operator <<(std::ostream& out, Lattice const& reticulo) {
+  for (int i = 0; i < reticulo.getSize(); ++i) {
+    Position posicion(i);
+    std::cout << reticulo.getCell(posicion);
+  }
+  std::cout << "  (" << reticulo.getN_generation() << ")\n";
+  return out;
+}
+
+void Lattice::nextGeneration() {
+  if (borderType_ != "periodica") {
+    for (int i = 1; i < size_ - 1; ++i) {
+      celulas_[i].setNextState(celulas_[i].NextState(*this));
+    }
+    for (int i = 1; i < size_ - 1; ++i) {
+      celulas_[i].updateState();
+    }
+  }
+  else {
+    for (int i = 0; i < size_; ++i) {
+      celulas_[i].setNextState(celulas_[i].NextState(*this));
+    }
+    for (int i = 0; i < size_; ++i) {
+      celulas_[i].updateState();
+    }
+  }
+  
+}
+
+void Lattice::updateN_generation() {
+  ++n_generation_;
+}
+
+Lattice::~Lattice() {
+  delete[] celulas_;
+}
